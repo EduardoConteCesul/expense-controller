e6c7303f7fddb16adfc9b18f3f5f41fff00c8157
EduardoConteCesul <eduardo.2024014337@alunocesul.com>
2025-09-11 21:08:03 -0300

    GostosView

--- src/main/java/br/com/expensecontroller/view/GastosView.java
+++ src/main/java/br/com/expensecontroller/view/GastosView.java
@@ -1,4 +1,29 @@
 package br.com.expensecontroller.view;
 
+import br.com.expensecontroller.enums.Categoria;
+import br.com.expensecontroller.model.Gastos;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+
+
 public class GastosView {
+
+    @FXML private ComboBox<Categoria> categoria;
+    @FXML private TextField descricao;
+    @FXML private TextField custo;
+    @FXML private DatePicker data;
+    @FXML private Button adicionar;
+    @FXML private Button remover;
+    @FXML private TableView<Gastos> tabelaDespesas;
+    @FXML private TableColumn<Gastos, Categoria> colCategoria;
+    @FXML private TableColumn<Gastos, String> colDescricao;
+    @FXML private TableColumn<Gastos, String> colCusto;
+    @FXML private TableColumn<Gastos, LocalDate> colData;
+
+    @FXML
+    private void initialize(){
+
+    }
 }
